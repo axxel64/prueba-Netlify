71d2a44d9eabfa107cb7bb60814b0438cdc0b352
axxel64 <alopsonic@hotmail.com>
2024-08-20 16:09:33 -0600

    tailwind configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "funko-blue": "#3A589E",
+        "form-title": "#63656b",
+        "btn-hover": "#f9fafb",
+        "wall-gray": "#F3F3F7",
+        "product-card": "#27272A",
+      },
+      backgroundImage: {
+        "funko-logo":
+          "url('https://cdn.icon-icons.com/icons2/2785/PNG/512/shopping_bag_icon_177404.png')",
+      },
+      backgroundSize: {
+        "funko-size": "10em", // Aquí defines el tamaño personalizado de la imagen de fondo.
+      },
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
+    screens: {
+      tablet: "1025px",
+      phone: "450px",
+    },
+    keyframes: {
+      bounceLeft: {
+        "0%, 100%": { transform: "translateX(0)" },
+        "50%": { transform: "translateX(-10px)" },
+      },
+    },
+    animation: {
+      bounceLeft: 'bounceLeft 0.5s ease-in-out',
+    },
   },
   plugins: [],
-}
-
+};
